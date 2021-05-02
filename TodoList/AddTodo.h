5546b1baee23cd5ae0fa22b8280886f23cb7f2c9
Андрей Кынтэрец <kynterets@gmail.com>
2021-05-02 08:27:42 +0400

    Added body to AddTodo.h

--- TodoList/AddTodo.h
+++ TodoList/AddTodo.h
@@ -10,11 +10,54 @@ Todo* AddTodo(Todo* oldArr, int *size)
 	delete[]oldArr;
 
 	Todo* newTodo = new Todo;
-	puts("<<< Создание нового дела: >>>");
-	puts("Введите название: ");
+	puts("\n<<< Создание нового дела: >>>");
+	puts("Как назовём?");
 	getchar();
-	cin.getline(newTodo->name, 50);
-	puts(newTodo->name);
+	gets_s(newTodo->name);
+
+	puts("\nВыберите приоритет:");
+	puts("1 - низкий");
+	puts("2 - средний");
+	puts("3 - высокий");
+	int selectedPriority;
+	while (1)
+	{
+		cin >> selectedPriority;
+		if (selectedPriority > 0 &&
+			selectedPriority <= 3)
+			break;
+
+		puts("\nОшибочный выбор!!! Повторите: ");
+	}
+
+	switch (selectedPriority)
+	{
+	case 1:
+		newTodo->priority = low;
+		break;
+	case 2:
+		newTodo->priority = middle;
+		break;
+	case 3:
+		newTodo->priority = middle;
+		break;
+	}
+
+	puts("\nОпишите: ");
+	getchar();
+	gets_s(newTodo->description);
+
+	puts("\nВведите дату и время исполнения:");
+	puts("День (DD): ");
+	cin >> newTodo->data.day;
+	puts("Месяц (MM): ");
+	cin >> newTodo->data.month;
+	puts("Год (YYYY): ");
+	cin >> newTodo->data.year;
+	puts("Час (HH): ");
+	cin >> newTodo->data.hour;
+	puts("Минуты (MM): ");
+	cin >> newTodo->data.minuts;
 
 	return newArr;
 }
--- TodoList/DelTodo.h
+++ TodoList/DelTodo.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 using namespace std;
 
 void DelTodo(Todo* arr, int* size)
