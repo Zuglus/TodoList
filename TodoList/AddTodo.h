47165e27bfa71536b0182b5a9498c5a56e0b9695
Андрей Кынтэрец <kynterets@gmail.com>
2021-05-08 17:14:06 +0400

    Add initialization to TodoStruct

--- TodoList/AddTodo.h
+++ TodoList/AddTodo.h
@@ -13,25 +13,30 @@ Todo* AddTodo(Todo* oldArr, int* size)
 	std::cout << "\n<<< Создание новой задачи: >>>\n";
 
 	std::cout << "\nЧто нужно сделать?: ";
-	newTodo->name = ConsoleEnter(50);
+	std::string* tempS = new std::string(ConsoleEnter(50));
+	if (tempS[0] != "\0")
+		newTodo->name = *tempS;
+	delete tempS;
 
 	std::cout << "\nВыберите приоритет:\n";
-	newTodo->priority = SetPriority();
+	tempS = new std::string(ConsoleEnter(50));
+	if (tempS[0] != "\0")
+		newTodo->priority = SetPriority();
+	delete tempS;
 
 	std::cout << "\nДобавьте подробности: ";
-	newTodo->description = ConsoleEnter(100);
+	tempS = new std::string(ConsoleEnter(100));
+	if (tempS[0] != "\0")
+		newTodo->description = *tempS;
+	delete tempS;
 
 	std::cout << "\nВведите дату и время исполнения:";
-	time_t now = time(NULL);
-	tm* tmpDate = new tm;
-	localtime_s(tmpDate, &now);
-
 	while (1)
 	{
 		char tmp[100];
 		int tmpInt;
 
-		std::cout << "\nДата (" << tmpDate->tm_mday << "): ";
+		std::cout << "\nДата (" << newTodo->date.tm_mday << "): ";
 		std::cin.getline(tmp, 100);
 		if (tmp[0] != '\0' &&
 			strlen(tmp) < 3 &&
@@ -40,10 +45,10 @@ Todo* AddTodo(Todo* oldArr, int* size)
 			tmpInt = atoi(tmp);
 			if (tmpInt > 0 &&
 				tmpInt <= 31)
-				tmpDate->tm_mday = tmpInt;
+				newTodo->date.tm_mday = tmpInt;
 		}
 
-		std::cout << "Месяц (" << tmpDate->tm_mon + 1 << "): ";
+		std::cout << "Месяц (" << newTodo->date.tm_mon + 1 << "): ";
 		std::cin.getline(tmp, 100);
 		if (tmp[0] != '\0' &&
 			strlen(tmp) < 3 &&
@@ -52,18 +57,18 @@ Todo* AddTodo(Todo* oldArr, int* size)
 			tmpInt = atoi(tmp);
 			if (tmpInt > 0 &&
 				tmpInt <= 12)
-				tmpDate->tm_mon = tmpInt - 1;
+				newTodo->date.tm_mon = tmpInt - 1;
 		}
 
-		std::cout << "Год (" << tmpDate->tm_year + 1900 << "): ";
+		std::cout << "Год (" << newTodo->date.tm_year + 1900 << "): ";
 		std::cin.getline(tmp, 100);
 		if (tmp[0] != '\0' &&
 			IsNumber(tmp))
 		{
-				tmpDate->tm_year = atoi(tmp);
+			newTodo->date.tm_year = atoi(tmp);
 		}
 
-		std::cout << "Часы (" << tmpDate->tm_hour << "): ";
+		std::cout << "Часы (" << newTodo->date.tm_hour << "): ";
 		std::cin.getline(tmp, 100);
 		if (tmp[0] != '\0' &&
 			strlen(tmp) < 3 &&
@@ -72,10 +77,10 @@ Todo* AddTodo(Todo* oldArr, int* size)
 			tmpInt = atoi(tmp);
 			if (tmpInt > 0 &&
 				tmpInt <= 23)
-				tmpDate->tm_hour = tmpInt;
+				newTodo->date.tm_hour = tmpInt;
 		}
 
-		std::cout << "Минуты (" << tmpDate->tm_min << "): ";
+		std::cout << "Минуты (" << newTodo->date.tm_min << "): ";
 		std::cin.getline(tmp, 100);
 		if (tmp[0] != '\0' &&
 			strlen(tmp) < 3 &&
@@ -84,18 +89,18 @@ Todo* AddTodo(Todo* oldArr, int* size)
 			tmpInt = atoi(tmp);
 			if (tmpInt > 0 &&
 				tmpInt < 60)
-				tmpDate->tm_min = tmpInt;
+				newTodo->date.tm_min = tmpInt;
 		}
 
-		tmpDate->tm_sec = 0;
+		newTodo->date.tm_sec = 0;
 
-		time_t inSeconds = mktime(tmpDate);
+
+		time_t inSeconds = mktime(&(*newTodo).date);
 		if (inSeconds > time(NULL))
 			break;
 		std::cout << "Ошибочная дата. Повторите...";
 	}
 
-	newTodo->date = *tmpDate;
 	newArr[(*size) - 1] = *newTodo;
 	delete newTodo;
 
--- TodoList/TodoStruct.h
+++ TodoList/TodoStruct.h
@@ -6,4 +6,15 @@ struct Todo
 	Priority priority;
 	std::string description;
 	tm date;
+	Todo()
+	{
+		name = "Новая задача";
+		priority = low;
+		description = "Описание новой задачи";
+		time_t now = time(NULL);
+		tm* tmpTime = new tm;
+		localtime_s(tmpTime, &now);
+		date = *tmpTime;
+		date.tm_hour++;
+	}
 };
